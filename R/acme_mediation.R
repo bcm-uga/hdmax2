4651f796828ec617aa21748b122f193f9a9edb74
Florence Pittion <florence.pittion@gmail.com>
2024-01-18 15:26:03 +0100

    add new files

---
+++ R/acme_mediation.R
@@ -0,0 +1,95 @@
+acme_mediation <- function(qval, X, Y, M, covar = NULL, U = NULL, FDR = 0.1, sims = 3, ...) {
+  
+  if (is.null(colnames(M))) {
+    colnames(M) <- 1:ncol(M)
+  }
+  
+  M <- M[, qval <= FDR]
+  
+  ##' Tables of results of mediation analyzes for markers with a Q-value below the FDR threshold.
+  ##' Composition of tables: estimated effect, confidence interval and mediation pValue.
+  ##'  - ACME, estimation of the average causal mediation effect (the indirect effect)
+  ##'  - ADE, estimation average direct effect
+  ##'  - PM, estimation of the proportion mediated
+  ##'  - TE, estimation of the total effect
+  # from package mediation
+  ACME <- matrix(ncol = 4, nrow = ncol(M))
+  ADE <- matrix(ncol = 4, nrow = ncol(M))
+  PM <- matrix(ncol = 4, nrow = ncol(M))
+  TE <- matrix(ncol = 4, nrow = ncol(M))
+  
+  # from linear models
+  xm <- matrix(ncol = 4, nrow = ncol(M))
+  my <- matrix(ncol = 4, nrow = ncol(M))
+  
+  for (i in 1:ncol(M)) {
+    
+    dat.x <- data.frame(X = X, Mi = M[, i], covar = cbind(covar, U))
+    dat.y <- data.frame(X = X, Mi = M[, i], covar = cbind(covar, U), Y = Y)
+    
+    # ici cas de deux reg lineaires pour les deux associations
+    # TODO refaire pour les autres regressions
+    mod1 <- stats::lm(Mi ~ X + ., data = dat.x)
+    mod2 <- stats::lm(Y ~ X + Mi + ., data = dat.y)
+    
+    # for linear models
+    xm[i, ] <- summary(mod1)$coeff[2, ] # effect of X
+    my[i, ] <- summary(mod2)$coeff[3, ] # effect of M
+    
+    med <- mediation::mediate(mod1, mod2, sims = sims, treat = "X", mediator = "Mi", ...)
+    
+    ACME[i, ] <- c(med$d0, med$d0.ci[1], med$d0.ci[2], med$d0.p)
+    ADE[i, ] <- c(med$z0, med$z0.ci[1], med$z0.ci[2], med$z0.p)
+    PM[i, ] <- c(med$n0, med$n0.ci[1], med$n0.ci[2], med$n0.p)
+    TE[i, ] <- c(med$tau.coef, med$tau0.ci[1], med$tau0.ci[2], med$tau.p)
+  }
+  
+  ACME <- as.data.frame(ACME)
+  ADE <- as.data.frame(ADE)
+  PM <- as.data.frame(PM)
+  TE <- as.data.frame(TE)
+  xm <- as.data.frame(xm)
+  my <- as.data.frame(my)
+  
+  colnames(ACME) <- c("est", "CI_2.5", "CI_97.5", "pval")
+  colnames(ADE) <- c("est", "CI_2.5", "CI_97.5", "pval")
+  colnames(PM) <- c("est", "CI_2.5", "CI_97.5", "pval")
+  colnames(TE) <- c("est", "CI_2.5", "CI_97.5", "pval")
+  colnames(xm) <- c("Estimate", "Std.Error", "t.Value", "pValue")
+  colnames(my) <- c("Estimate", "Std.Error", "t.Value", "pValue")
+  
+  ACME$feat <- colnames(M)
+  ADE$feat <- colnames(M)
+  PM$feat <- colnames(M)
+  TE$feat <- colnames(M)
+  xm$feat <- colnames(M)
+  my$feat <- colnames(M)
+  
+  return(list(ACME = ACME,
+              ADE = ADE,
+              PM = PM,
+              TE = TE,
+              xm = xm,
+              my = my))
+  
+}
+
+
+plot_summary_ACME <- function(ACME) {
+  
+  # for check problem
+  
+  p <- ggplot(res_med$ACME, aes(est, stats::reorder(feat, est), color = pval <= 0.05, shape = pval <= 0.05)) +
+    geom_vline(xintercept = 0, linetype = "dashed") +
+    geom_errorbarh(aes(xmin = CI_2.5, xmax = CI_97.5)) +
+    geom_point(size = 0.8) +
+    theme_bw() +
+    xlab("ACME (Average Causal Mediation Effect)") +
+    ylab("CpG") +
+    theme(panel.border = element_blank(),
+          panel.spacing = unit(0.01, "lines"),
+          axis.ticks = element_blank()) +
+    scale_color_manual(values = c("black", "red"))
+  
+  print(p)
+}
---
+++ R/runAS.R
@@ -0,0 +1,172 @@
+runAS = function(X_matrix,
+                 Y_matrix,
+                 M_matrix, 
+                 K,
+                 X_type,
+                 Y_type,
+                 M_type,
+                 conf = NULL,
+                 diagnostic.plot = F #,
+                 #genomic.control = T
+                 ) {
+  res = list()
+  
+    if(X_type=="continuous"){
+    #regression 1: M ~ X
+    mod.lfmm1 = LEA::lfmm2(input = M_matrix, 
+                           env = X_matrix, 
+                           K=5)
+    res_ewas1 = LEA::lfmm2.test(mod.lfmm1, 
+                                input = M_matrix, 
+                                env = X_matrix,
+                                genomic.control = T)
+    pval1 = as.double(res_ewas1$pvalues)
+    names(pval1) = colnames(M)
+    length(pval1)
+    U1 = mod.lfmm1@U
+    V1 = mod.lfmm1@V
+    effect.sizes1 = mod.lfmm1@B
+    lambda1 = mod.lfmm1@lambda
+    
+    zscores1 = res_ewas1$zscores
+    fscores1 = res_ewas1$fscores
+    #adj_rsquared1 = res_ewas1$adj.r.squared
+    gif1 = res_ewas1$gif
+    reg1 = list(pval1,
+                U1, 
+                V1, 
+                effect.sizes1,
+                lambda1, 
+                zscores1,
+                fscores1,
+                #adj_rsquared1 = res_ewas1$adj.r.squared
+                gif1)
+    names(reg1) = c("pval","U","V","effect.sizes","lambda","zscores","fscores","gif")
+  }
+  
+
+  if(X_type=="binary"){
+    mod.lfmm1 = LEA::lfmm2(input = M_matrix, 
+                           env = X_matrix, 
+                           K=5)
+    res_ewas1 = LEA::lfmm2.test(mod.lfmm1, 
+                                input = M_matrix, 
+                                env = X_matrix,
+                                genomic.control = T)
+    pval1 = as.double(res_ewas1$pvalues)
+    names(pval1) = colnames(M)
+    length(pval1)
+    U1 = mod.lfmm1@U
+    V1 = mod.lfmm1@V
+    effect.sizes1 = mod.lfmm1@B
+    lambda1 = mod.lfmm1@lambda
+    
+    zscores1 = res_ewas1$zscores
+    fscores1 = res_ewas1$fscores
+    #adj_rsquared1 = res_ewas1$adj.r.squared
+    gif1 = res_ewas1$gif
+    reg1 = list(pval1,
+                U1, 
+                V1, 
+                effect.sizes1, 
+                lambda1, 
+                zscores1,
+                fscores1,
+                #adj_rsquared1 = res_ewas1$adj.r.squared
+                gif1)
+    names(reg1) = c("pval","U","V","effect.sizes","lambda","zscores","fscores","gif")
+  }
+  
+  res[[1]] = reg1
+  
+  ## TODO  
+  # if(X_type=="categorial")
+  #     #else ...
+  
+  if(Y_type=="continuous"){
+    mod.lfmm2 = LEA::lfmm2(input = M_matrix, 
+                           env = cbind(X_matrix, Y_matrix, conf), 
+                           K=5)
+    res_ewas2 = LEA::lfmm2.test(mod.lfmm2, 
+                                input = M_matrix, 
+                                env = cbind(X_matrix, Y_matrix, conf),
+                                genomic.control = T,
+                                full = T)
+    pval2 = as.double(res_ewas2$pvalues)
+    names(pval2) = colnames(M)
+    length(pval2)
+    U2 = mod.lfmm2@U
+    V2 = mod.lfmm2@V
+    effect.sizes2 = mod.lfmm2@B
+    lambda2 = mod.lfmm2@lambda
+    
+    zscores2 = res_ewas2$zscores
+    fscores2 = res_ewas2$fscores
+    #adj_rsquared2 = res_ewas2$adj.r.squared
+    gif2 = res_ewas2$gif
+    reg2 = list(pval2,
+                U2, 
+                V2, 
+                effect.sizes2, 
+                lambda2, 
+                zscores2,
+                fscores2,
+                #adj_rsquared2 = res_ewas2$adj.r.squared
+                gif2)
+    names(reg2) = c("pval", "U", "V", "effect.sizes", "lambda", "zscores", "fscores", "gif")
+  }
+  res[[2]] = reg2  
+  # if(Y_type=="binary"){
+  # }
+  
+  
+  # if(Y_type=="surv_Cox"){
+  #   #Y = survival::Surv(OT, status)
+  #   #regression 2: Y ~ M +X
+  #   p = ncol(M)
+  #   pval2 = c()
+  #   for (j in 1:p) {
+  #     #cox_model = survival::coxph(Y ~ M[,j] + X)
+  #     cox_model = survival::coxph(Y ~ M[,j] + X + mod.lfmm@U)
+  #     pval2 = c(pval2, summary(cox_model)$coefficients[1,5])
+  #     # beta_est = c(beta_est, summary(cox_model)$coefficients)
+  #   }
+  #   names(pval2)=  colnames(M)
+  # }
+  # if(Y_type=="surv_AG")
+  
+  # TODO transform pval in calibrated pval
+  
+  # max2 test
+  max2_pval <- apply(cbind(pval1, pval2), 1, max)^2
+  eta0 <- fdrtool::pval.estimate.eta0(max2_pval, diagnostic.plot = diagnostic.plot)
+  qval <- fdrtool::fdrtool(max2_pval,statistic = "pvalue", plot = F, verbose = F)$qval
+  max2 = list(max2_pval, eta0, qval)
+  names(max2) = c("pval", "eta0", "qval")
+  res[[3]] = max2
+  
+  names(res) <- c("mod1", "mod2", "max2")
+  return(res)
+  # return(list(calibrated_pval1 = pval1, 
+  #             calibrated_pval2 = pval2, 
+  #             U1 = U1,
+  #             V1 = V1,
+  #             effect.sizes1 = effect.sizes1,
+  #             lambda1 = lambda1,
+  #             zscores1 = zscores1,
+  #             fscores1 = fscores1,
+  #             #adj_rsquared1 = adj.r.squared1,
+  #             gif1 = gif1,
+  #             U2 = U2,
+  #             V2 = V2,
+  #             effect.sizes2 = effect.sizes2,
+  #             lambda2 = lambda2,
+  #             zscores2 = zscores2,
+  #             fscores2 = fscores2,
+  #             #adj_rsquared2 = adj.r.squared2,
+  #             gif2 = gif2,
+  #             max2_pval = max2_pval,
+  #             max2_eta0 = eta0,
+  #             max2_qval = qval
+  # ))
+}
