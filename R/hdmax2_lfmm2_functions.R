4fc9375f70f9b920b793b81285a594497f957b8c
Florence Pittion <florence.pittion@gmail.com>
2024-03-18 19:59:45 +0100

    remove LEA dependencie

--- R/hdmax2_lfmm2_functions.R
+++ R/hdmax2_lfmm2_functions.R
@@ -323,3 +323,58 @@ lfmm2_med_test= function(object,
   res <- list(pvalues = p_value, zscores = z_score, fscores = f_score, adj.r.squared = r_squared,  gif = gif)
   return(res)
 }
+
+
+
+
+
+read.env <- function(input.file) {
+  
+  # test arguments
+  if(missing(input.file))
+    stop("'input.file' argument is missing.")
+  else if (!is.character(input.file))
+    stop("'input.file' argument has to be of type character.")
+  # check extension 
+  test_extension(input.file, "env")
+  
+  return(as.matrix(read.table(input.file)));
+}
+
+
+
+read.lfmm <- function(input.file) {
+  
+  # test arguments
+  if(missing(input.file))
+    stop("'input.file' argument is missing.")
+  else if (!is.character(input.file))
+    stop("'input.file' argument has to be of type character.")
+  # check extension 
+  test_extension(input.file, "lfmm")
+  
+  return(as.matrix(read.table(input.file)))
+}
+
+
+
+test_extension <- function(name, extension)
+{
+  # obtain the extension of name
+  ext = getExtension(basename(name))
+  
+  # if not the correct extension, stop
+  if (ext != extension) {
+    p = paste("'input_file' format and extension have to be \".", 
+              extension, "\" (not \".",ext,"\").", sep="")
+    stop(p)
+  } 
+  
+  return(ext);
+}
+
+getExtension <- function(file)
+{
+  l = strsplit(file, "\\.")[[1]]
+  return(l[length(l)])
+}
